8fb3fec8e587218fbb390a01c50c51f4dc825e4f
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-04-21 09:17:54 +0900

    サイト全体フォント"M PLUS 1p"へ変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import { BrowserRouter } from "react-router-dom";
-
 import "./App.css";
-
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 import { MainRouter } from "./router/MainRouter";
 
 export const App = () => {
+  const apptheme = createTheme({
+    typography: {
+      fontFamily: ["M PLUS 1p", "sans-serif"].join(","),
+    },
+  });
   return (
-    <BrowserRouter>
-      <MainRouter />
-    </BrowserRouter>
+    <ThemeProvider theme={apptheme}>
+      <BrowserRouter>
+        <MainRouter />
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
