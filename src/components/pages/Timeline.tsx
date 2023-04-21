b2681240a0682ce5b892ec64b54c0b758aee8bf2
Kazuaki Kuwahara <kazuaki.kuwahara@rakus-partners.co.jp>
2023-04-21 10:49:20 +0900

    modal

--- src/components/pages/Timeline.tsx
+++ src/components/pages/Timeline.tsx
@@ -1,12 +1,35 @@
 import { FC, memo } from "react";
+import ModalWindow from "../organisms/ModalWindow";
 
 type Props = {};
 
 const Timeline: FC<Props> = memo((props) => {
   return (
-    <>
-
-    </>
+    <div>
+      <ModalWindow
+      title="投稿"
+      content="投稿します。よろしいですか？"
+      openButtonColor="blue"
+      openButtonSxStyle={{pb: '100px'}}
+      completeButtonColor="blue"
+      completeButtonName="投稿"
+      completeAction={() => {
+        console.log("complete");
+      }}
+      cancelButtonColor="orange"
+    />
+    <ModalWindow
+      title="投稿削除"
+      content="投稿を削除します。よろしいですか？"
+      openButtonColor="red"
+      completeButtonColor="blue"
+      completeButtonName="削除"
+      completeAction={() => {
+        console.log("delete");
+      }}
+      cancelButtonColor="pink"
+    />
+    </div>
   );
 });
 
