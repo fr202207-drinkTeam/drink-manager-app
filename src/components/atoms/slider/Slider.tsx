eb29f4514e1d16b0bfc50a58d0cd9d6518626600
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-04-24 11:55:32 +0900

    sliderコンポーネント作成

---
+++ src/components/atoms/slider/Slider.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+import { Autoplay, Swiper as RealSwiper } from "swiper";
+RealSwiper.use([Autoplay]);
+type Props = {
+  images: string[];
+  slidesPerView: number;
+  loop: boolean;
+  autoplay: any;
+  navigation: boolean;
+};
+const Slider = (props: Props) => {
+  const { images, slidesPerView, loop, autoplay, navigation } = props;
+  return (
+    <Swiper
+      modules={[Navigation, Pagination, Scrollbar]}
+      slidesPerView={slidesPerView}
+      loop={loop}
+      pagination={{ clickable: true }}
+      navigation={navigation}
+      autoplay={autoplay}
+      className="itemSwiper"
+    >
+      {images.map((image: string, index: any) => (
+        <SwiperSlide key={index}>
+          <img src={image} alt="" style={{ width: "100%" }} />
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+};
+
+export default Slider;
