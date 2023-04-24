28cd85e79bd983227385f79aeef3df0b3482cd00
kayahara mai <mai.kayahara@rakus-partners.co.jp>
2023-04-24 09:00:54 +0900

    パス修正

--- src/components/pages/Consumption.tsx
+++ src/components/pages/Consumption.tsx
@@ -1,53 +1,53 @@
-import { Box, Button, Modal, Paper, Typography } from '@mui/material';
-import { FC, memo } from 'react';
-import StockCard from '../card/StockCard';
-import { PrimaryButton } from '../button/Button';
-import useGetOfficeItems from '../../hooks/useGetOfficeItems';
-import { useState, useEffect } from 'react';
+import { Box, Button, Modal, Paper, Typography } from "@mui/material";
+import { FC, memo } from "react";
+import StockCard from "../card/StockCard";
+import { PrimaryButton } from "../atoms/button/Button";
+import useGetOfficeItems from "../../hooks/useGetOfficeItems";
+import { useState, useEffect } from "react";
 
 type Props = {};
 
 const Consumption: FC<Props> = memo((props) => {
   const officeItemData = useGetOfficeItems({ intheOffice: true });
-  console.log(officeItemData)
+  console.log(officeItemData);
   return (
     <Paper
       sx={{
         mb: 5,
-        width: '100%',
+        width: "100%",
         minWidth: 500,
         maxWidth: 1200,
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
       }}
     >
       <Typography
         sx={{
-          textAlign: 'center',
-          fontFamily: 'Georgia',
-          fontSize: '50px',
-          color: '#024098',
-          mt: '20px',
+          textAlign: "center",
+          fontFamily: "Georgia",
+          fontSize: "50px",
+          color: "#024098",
+          mt: "20px",
         }}
       >
         -消費在庫入力-
       </Typography>
       <StockCard officeItemData={officeItemData} />
-      <div style={{ display: 'inline-flex' }}>
+      <div style={{ display: "inline-flex" }}>
         <Button
           sx={{
-            background: '#024098',
-            color: '#FFF',
-            fontWeight: 'bold',
+            background: "#024098",
+            color: "#FFF",
+            fontWeight: "bold",
             px: 10,
             py: 4,
             my: 5,
             borderRadius: 20,
             fontSize: 20,
-            direction: 'rtl',
-            width: '300px',
+            direction: "rtl",
+            width: "300px",
           }}
         >
           送信
