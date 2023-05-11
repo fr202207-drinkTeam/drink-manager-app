c308c390ecad306e48328909cc750230b1003e80
“Kanako_Watanabe” <109854906+watanabekanako@users.noreply.github.com>
2023-05-12 08:34:11 +0900

    fqq react chat botインストール

--- src/components/pages/Faq.tsx
+++ src/components/pages/Faq.tsx
@@ -1,13 +1,41 @@
-import { FC, memo } from "react";
+import React from "react";
+import ChatBot from "react-simple-chatbot";
 
-type Props = {};
-
-const Faq: FC<Props> = memo((props) => {
+export const Faq = () => {
+  const style = {
+    width: "100%　!important",
+    margin: "0 auto",
+    marginTop: 0,
+  };
   return (
-    <>
-
-    </>
+    <div style={style}>
+      <div style={{ width: "100% !important" }}>
+        <ChatBot
+          className="custom-chatbot"
+          headerTitle="チャットbot"
+          recognitionEnable={true}
+          steps={[
+            {
+              id: "1",
+              message: "質問はありますか?",
+              trigger: "2",
+            },
+            {
+              id: "2",
+              user: true,
+              trigger: "3",
+            },
+            {
+              id: "3",
+              message:
+                "{previousValue}ですね。こちらはお問い合わせフオームをご利用ください。",
+              end: true,
+            },
+          ]}
+        />
+      </div>
+    </div>
   );
-});
+};
 
 export default Faq;
