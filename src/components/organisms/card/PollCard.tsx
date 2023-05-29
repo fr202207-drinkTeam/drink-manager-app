344ca7cc02d84daacf33faebf9c2d48be901fb5c
sasaki mai <mai.sasaki@rakus-partners.co.jp>
2023-05-29 09:08:26 +0900

    投票終了したことが分かりやすいように表示変更

--- src/components/organisms/card/PollCard.tsx
+++ src/components/organisms/card/PollCard.tsx
@@ -45,21 +45,21 @@ const PollCard = ({ data, pollNum, pollCategory, sxStyle }: PollCardProps) => {
   });
 
   //現在表示されている人気投票アンケートに投票しているか
-const isPopularQuestionnaireData =popularData.filter((p)=>{
- return p.questionnaireId===pollNum
-})
+  const isPopularQuestionnaireData = popularData.filter((p) => {
+    return p.questionnaireId === pollNum
+  })
   //ユーザが今表示されている人気投票に投票した商品
-const popularItem=isPopularQuestionnaireData.map((p)=>{
-return p.result
-})
+  const popularItem = isPopularQuestionnaireData.map((p) => {
+    return p.result
+  })
   //現在表示されているその他投票アンケートに投票しているか
-const isOthersQuestionnaireData=othersData.filter((o)=>{
-  return o.questionnaireId===pollNum
-})
+  const isOthersQuestionnaireData = othersData.filter((o) => {
+    return o.questionnaireId === pollNum
+  })
   //ユーザが今表示されているその他投票に投票した商品
-const othersItem=isOthersQuestionnaireData.map((o)=>{
-return o.result
-})
+  const othersItem = isOthersQuestionnaireData.map((o) => {
+    return o.result
+  })
 
   //投票ボタン
   const submitPoll = async (drinkId: number) => {
@@ -96,6 +96,7 @@ return o.result
           ml: 3,
         }}
       >
+
         {data &&
           data.map((drink: Items, index) => {
             return (
@@ -110,114 +111,253 @@ return o.result
                 }}
                 key={index}
               >
-                <CardActionArea component="a" href={`/home/search/${drink.id}`}>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                    sx={{
-                      textAlign: "center",
-                      fontSize: "13px",
-                      backgroundColor: "#d6c6af",
-                      width: 80,
-                      p: "3px",
-                      color: "#000",
-                      borderRadius: "3px",
-                    }}
-                  >
-                    {(() => {
-                      if (
-                        Number(drink.itemCategory) >= 1 &&
-                        Number(drink.itemCategory) <= 4
-                      ) {
-                        return "コーヒー";
-                      } else if (drink.itemCategory === 5) {
-                        return "ティー";
-                      } else if (drink.itemCategory === 6) {
-                        return "ココア";
-                      } else {
-                        return "その他";
-                      }
-                    })()}
-                  </Typography>
-                  <CardActionArea>
-                    <CardMedia
-                      component="img"
-                      alt="商品画像"
-                      height="140"
-                      width="140"
-                      image={drink.image[0]}
-                      title="商品名"
-                      sx={{
-                        display: "block",
-                        width: 200,
-                        height: 200,
-                        objectFit: "cover",
-                        m: "auto",
-                        p: 1,
-                      }}
-                    />
-                    <CardContent sx={{ height: "150px" }}>
-                      {drink.intheOffice ? (
-                        <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          component="p"
-                          sx={{
-                            textAlign: "center",
-                            fontSize: "13px",
-                            backgroundColor: "#e0ebaf",
-                            width: 80,
-                            p: "3px",
-                            color: "#000",
-                            borderRadius: "3px",
-                          }}
-                        >
-                          社内あり
-                        </Typography>
-                      ) : (
-                        <Typography
-                          variant="body2"
-                          color="textSecondary"
-                          component="p"
-                          sx={{
-                            textAlign: "center",
-                            fontSize: "13px",
-                            backgroundColor: "#a4c1d7",
-                            width: 80,
-                            p: "3px",
-                            color: "#000",
-                            borderRadius: "3px",
-                          }}
-                        >
-                          社内なし
-                        </Typography>
-                      )}
-                      <Typography
-                        gutterBottom
-                        sx={{
-                          textAlign: "center",
-                          fontSize: "14px",
-                          borderBottom: "double",
-                          fontFamily: "Georgia",
-                          fontWeight: "bold",
-                          height: "200",
-                          mt: 1,
-                        }}
-                      >
-                        {drink.name}
-                      </Typography>
+                {(popularItem[0] === drink.id) || (othersItem[0] === drink.id) ?
+                  (
+                    <CardActionArea component="a" href={`/home/search/${drink.id}`}>
                       <Typography
                         variant="body2"
                         color="textSecondary"
                         component="p"
-                        sx={{ textAlign: "center", fontSize: "13px" }}
+                        sx={{
+                          textAlign: "center",
+                          fontSize: "13px",
+                          backgroundColor: "#d6c6af",
+                          width: 80,
+                          p: "3px",
+                          color: "#000",
+                          borderRadius: "3px",
+                        }}
                       >
-                        {drink.description}
+                        {(() => {
+                          if (
+                            Number(drink.itemCategory) >= 1 &&
+                            Number(drink.itemCategory) <= 4
+                          ) {
+                            return "コーヒー";
+                          } else if (drink.itemCategory === 5) {
+                            return "ティー";
+                          } else if (drink.itemCategory === 6) {
+                            return "ココア";
+                          } else {
+                            return "その他";
+                          }
+                        })()}
                       </Typography>
-                    </CardContent>
-                  </CardActionArea>
-                </CardActionArea>
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          alt="商品画像"
+                          height="140"
+                          width="140"
+                          image={drink.image[0]}
+                          title="商品名"
+                          sx={{
+                            display: "block",
+                            width: 200,
+                            height: 200,
+                            objectFit: "cover",
+                            m: "auto",
+                            p: 1,
+                          }}
+                        />
+                        <Box
+                          sx={{
+                            position: "absolute",
+                            top: 0,
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            bgcolor: "RGB(238, 232, 170,0.3)",
+                          }}
+                        />
+                        <Typography
+                          variant="h5"
+                          sx={{
+                            position: "absolute",
+                            top: "50%",
+                            left: "50%",
+                            transform: "translate(-50%, -50%)",
+                            color: "#fff",
+                            zIndex: 1,
+                            textAlign: "center",
+                            width: "100%",
+                          }}
+                        >
+                        </Typography>
+                        <CardContent sx={{ height: "150px" }}>
+                          {drink.intheOffice ? (
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                              sx={{
+                                textAlign: "center",
+                                fontSize: "13px",
+                                backgroundColor: "#e0ebaf",
+                                width: 80,
+                                p: "3px",
+                                color: "#000",
+                                borderRadius: "3px",
+                              }}
+                            >
+                              社内あり
+                            </Typography>
+                          ) : (
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                              sx={{
+                                textAlign: "center",
+                                fontSize: "13px",
+                                backgroundColor: "#a4c1d7",
+                                width: 80,
+                                p: "3px",
+                                color: "#000",
+                                borderRadius: "3px",
+                              }}
+                            >
+                              社内なし
+                            </Typography>
+                          )}
+                          <Typography
+                            gutterBottom
+                            sx={{
+                              textAlign: "center",
+                              fontSize: "14px",
+                              borderBottom: "double",
+                              fontFamily: "Georgia",
+                              fontWeight: "bold",
+                              height: "200",
+                              mt: 1,
+                            }}
+                          >
+                            {drink.name}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                            sx={{ textAlign: "center", fontSize: "13px" }}
+                          >
+                            {drink.description}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </CardActionArea>
+                  )
+                  :
+                  (
+                    <CardActionArea component="a" href={`/home/search/${drink.id}`}>
+                      <Typography
+                        variant="body2"
+                        color="textSecondary"
+                        component="p"
+                        sx={{
+                          textAlign: "center",
+                          fontSize: "13px",
+                          backgroundColor: "#d6c6af",
+                          width: 80,
+                          p: "3px",
+                          color: "#000",
+                          borderRadius: "3px",
+                        }}
+                      >
+                        {(() => {
+                          if (
+                            Number(drink.itemCategory) >= 1 &&
+                            Number(drink.itemCategory) <= 4
+                          ) {
+                            return "コーヒー";
+                          } else if (drink.itemCategory === 5) {
+                            return "ティー";
+                          } else if (drink.itemCategory === 6) {
+                            return "ココア";
+                          } else {
+                            return "その他";
+                          }
+                        })()}
+                      </Typography>
+                      <CardActionArea>
+                        <CardMedia
+                          component="img"
+                          alt="商品画像"
+                          height="140"
+                          width="140"
+                          image={drink.image[0]}
+                          title="商品名"
+                          sx={{
+                            display: "block",
+                            width: 200,
+                            height: 200,
+                            objectFit: "cover",
+                            m: "auto",
+                            p: 1,
+                          }}
+                        />
+                        <CardContent sx={{ height: "150px" }}>
+                          {drink.intheOffice ? (
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                              sx={{
+                                textAlign: "center",
+                                fontSize: "13px",
+                                backgroundColor: "#e0ebaf",
+                                width: 80,
+                                p: "3px",
+                                color: "#000",
+                                borderRadius: "3px",
+                              }}
+                            >
+                              社内あり
+                            </Typography>
+                          ) : (
+                            <Typography
+                              variant="body2"
+                              color="textSecondary"
+                              component="p"
+                              sx={{
+                                textAlign: "center",
+                                fontSize: "13px",
+                                backgroundColor: "#a4c1d7",
+                                width: 80,
+                                p: "3px",
+                                color: "#000",
+                                borderRadius: "3px",
+                              }}
+                            >
+                              社内なし
+                            </Typography>
+                          )}
+                          <Typography
+                            gutterBottom
+                            sx={{
+                              textAlign: "center",
+                              fontSize: "14px",
+                              borderBottom: "double",
+                              fontFamily: "Georgia",
+                              fontWeight: "bold",
+                              height: "200",
+                              mt: 1,
+                            }}
+                          >
+                            {drink.name}
+                          </Typography>
+                          <Typography
+                            variant="body2"
+                            color="textSecondary"
+                            component="p"
+                            sx={{ textAlign: "center", fontSize: "13px" }}
+                          >
+                            {drink.description}
+                          </Typography>
+                        </CardContent>
+                      </CardActionArea>
+                    </CardActionArea>
+                  )}
+
                 <ActiveBeigeButton
                   onClick={() => {
                     navigate(`/home/search/${drink.id}`);
@@ -243,20 +383,20 @@ return o.result
                   PopularPollData.some(
                     (data) => data.questionnaireId === pollNum
                   )) ||
-                (othersData.length >= 1 &&
-                  pollCategory === 2 &&
-                  OthersPollData.some(
-                    (data) => data.questionnaireId === pollNum
-                  )) ||
-                (loginUser.isAdmin === true )? (
-                  (popularItem[0]===drink.id)||(othersItem[0]===drink.id)?(   <InactiveButton
+                  (othersData.length >= 1 &&
+                    pollCategory === 2 &&
+                    OthersPollData.some(
+                      (data) => data.questionnaireId === pollNum
+                    )) ||
+                  (loginUser.isAdmin === true) ? (
+                  (popularItem[0] === drink.id) || (othersItem[0] === drink.id) ? (<InactiveButton
                     sx={{
                       background: "#e29399",
                       width: 200,
                       textAlign: "center",
                       mb: 2,
                       boxShadow: "none",
-                      border: "solid 2px red",
+                      border: "dotted 2px",
                       fontWeight: "bold",
                       ml: 6.5,
                       ":hover": {
@@ -266,7 +406,7 @@ return o.result
                     }}
                   >
                     &nbsp;投票した商品です
-                  </InactiveButton>):(   <InactiveButton
+                  </InactiveButton>) : (<InactiveButton
                     sx={{
                       background: "#e29399",
                       width: 200,
@@ -284,7 +424,7 @@ return o.result
                   >
                     &nbsp;投票完了
                   </InactiveButton>)
-                 
+
                 ) : (
                   <ModalWindow
                     title={`${drink.name}に投票してもよろしいですか？？`}
