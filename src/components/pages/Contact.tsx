f95d02288d00505c8f3aee451a1b273f075a6687
Yuka Naito <yuka.naito@rakus-partners.co.jp>
2023-06-02 11:43:44 +0900

    pullのため

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -7,114 +7,128 @@ import {
   TextField,
   Select,
   MenuItem,
-} from '@mui/material';
-import { FC, memo, useState } from 'react';
-import { PrimaryButton } from '../atoms/button/Button';
-import ModalWindow from '../organisms/ModalWindow';
-import { useNavigate } from 'react-router-dom';
+  InputLabel,
+  FormHelperText,
+  FormControl,
+} from "@mui/material";
+import { FC, memo, useState } from "react";
+import { InactiveButton, PrimaryButton } from "../atoms/button/Button";
+import ModalWindow from "../organisms/ModalWindow";
+import { useNavigate } from "react-router-dom";
+import { PrimaryInput } from "../atoms/input/Input";
 
 type Props = {};
 
 const Contact: FC<Props> = memo((props) => {
-  const [inquiryType, setInquiryType] = useState('');
+  const [contentErr, setContentErr] = useState<boolean>(false)
+  const [contactCategory, setContactCategory] = useState<number>(0);
+  const [contactContents, setContactContents] = useState<string>("");
+
   const navigate = useNavigate();
 
-  const handleInquiryTypeChange = (event: any) => {
-    setInquiryType(event.target.value as string);
-  };
+  // const handleInquiryTypeChange = (event: any) => {
+  //   setInquiryType(event.target.value as string);
+  // };
 
   const onClickSubmit = () => {
-    navigate('/home');
+    navigate("/home");
   };
 
   return (
     <Paper
       sx={{
-        padding: '50px 100px',
+        padding: "50px 100px",
       }}
     >
       <Box id="top" />
-      <Stack
-        direction="column"
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        spacing={4}
-      >
+      <Box>
         <Typography
-          sx={{
-            fontFamily: 'Georgia',
-            fontSize: '50px',
-            mt: '20px',
-          }}
+          variant="h3"
+          borderBottom="1px solid"
+          gutterBottom
+          sx={{ mb: "100px" }}
         >
           お問い合わせ
         </Typography>
-        <Stack
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-          spacing={10}
-        >
-          <Typography sx={{ minWidth: '160px' }}>お問い合わせ種別：</Typography>
-          <Select
-            value={inquiryType}
-            onChange={handleInquiryTypeChange}
-            sx={{ minWidth: '200px' }}
-          >
-            <MenuItem value="system">システムについてのお問い合わせ</MenuItem>
-            <MenuItem value="service">サービスについてのお問い合わせ</MenuItem>
-            <MenuItem value="coffee">
-              コーヒーサーバについてのお問い合わせ
-            </MenuItem>
-            <MenuItem value="other">その他</MenuItem>
-          </Select>
-        </Stack>
-        <Stack
-          direction="row"
-          justifyContent="flex-start"
-          alignItems="center"
-          spacing={10}
-        >
-          <Typography sx={{ minWidth: '160px' }}>お問い合わせ内容：</Typography>
-          <Box>
-            <TextField
-              sx={{ width: 500 }}
-              name="fbmessage"
-              id="fbmessage"
-              multiline
-              minRows={6}
-              variant="filled"
-              margin="dense"
-              size="small"
+        <Box sx={{ mb: "30px" }}>
+          <FormControl variant="standard" error={contactCategory === 0}>
+            <InputLabel id="itemCategoryField" required>
+              お問い合わせ種別
+            </InputLabel>
+            <Select
+              labelId="contactCategoryField"
+              id="contactCategoryField"
+              value={contactCategory}
+              label="お問い合わせ種別"
+              onChange={(e) => {
+                setContactCategory(Number(e.target.value));
+              }}
+            >
+              <MenuItem value={0}>お問い合わせ種別を選択</MenuItem>
+              <MenuItem value={1}>システムについてのお問い合わせ</MenuItem>
+              <MenuItem value={2}>サービスについてのお問い合わせ</MenuItem>
+              <MenuItem value={3}>
+                コーヒーサーバについてのお問い合わせ
+              </MenuItem>
+              <MenuItem value={4}>その他</MenuItem>
+            </Select>
+            {contactCategory === 0 && (
+              <FormHelperText>お問い合わせ種別を選択して下さい</FormHelperText>
+            )}
+          </FormControl>
+        </Box>
+        <PrimaryInput
+          multiline
+          aria-label="contactContents"
+          label="お問い合わせ内容"
+          sx={{ width: "100%", mb: "60px" }}
+          value={contactContents}
+          required
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setContactContents(e.target.value)
+          }
+          rows={5}
+          helperText={
+            contentErr && contactContents === "" && "お問い合わせ内容を入力して下さい"
+          }
+          error={contentErr && contactContents === ""}
+          onBlur={() => setContentErr(true)}
+        />
+
+        <Box sx={{ display: "flex", justifyContent: "center", mb: "60px" }}>
+          {contactCategory === 0 ? (
+            <InactiveButton
+              sxStyle={{
+                px: 7,
+                py: 2,
+                borderRadius: "15px",
+                marginTop: "32px",
+                textAlign: "center",
+              }}
+            >
+              送信
+            </InactiveButton>
+          ) : (
+            <ModalWindow
+              title="送信します、よろしいですか？"
+              content={""}
+              openButtonColor="blue"
+              buttonName="送信"
+              completeButtonColor={"blue"}
+              completeButtonName={`はい`}
+              completeAction={onClickSubmit}
+              cancelButtonColor={"red"}
+              openButtonSxStyle={{
+                px: 7,
+                py: 2,
+                borderRadius: "15px",
+                marginTop: "32px",
+                textAlign: "center",
+              }}
             />
-          </Box>
-        </Stack>
-        <Stack
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          sx={{ width: '100%' }}
-        >
-          <ModalWindow
-            title="送信します、よろしいですか？"
-            content={''}
-            openButtonColor="blue"
-            buttonName="送信"
-            completeButtonColor={'blue'}
-            completeButtonName={`はい`}
-            completeAction={onClickSubmit}
-            cancelButtonColor={'red'}
-            openButtonSxStyle={{
-              px: 9,
-              py: 3,
-              borderRadius: '32px',
-              marginTop: '32px',
-              textAlign: 'center',
-              width: '100%',
-            }}
-          />
-        </Stack>
-      </Stack>
+          )}
+        </Box>
+      </Box>
     </Paper>
   );
 });
