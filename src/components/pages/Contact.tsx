df6026b1dd0a1c29e8d6abaa6e2ea7c8c0022999
Yuka Naito <yuka.naito@rakus-partners.co.jp>
2023-06-02 14:47:11 +0900

    お問い合わせ機能実装

--- src/components/pages/Contact.tsx
+++ src/components/pages/Contact.tsx
@@ -1,37 +1,30 @@
-import {
-  Paper,
-  Typography,
-  Input,
-  Stack,
-  Box,
-  TextField,
-  Select,
-  MenuItem,
-  InputLabel,
-  FormHelperText,
-  FormControl,
-} from "@mui/material";
+import { Paper, Typography, Box } from "@mui/material";
 import { FC, memo, useState } from "react";
-import { InactiveButton, PrimaryButton } from "../atoms/button/Button";
+import { InactiveButton } from "../atoms/button/Button";
 import ModalWindow from "../organisms/ModalWindow";
 import { useNavigate } from "react-router-dom";
-import { PrimaryInput } from "../atoms/input/Input";
+import ContactForm from "../organisms/ContactForm";
 
-type Props = {};
-
-const Contact: FC<Props> = memo((props) => {
-  const [contentErr, setContentErr] = useState<boolean>(false)
-  const [contactCategory, setContactCategory] = useState<number>(0);
+const Contact: FC = memo(() => {
+  const [contactCategory, setContactCategory] = useState<string>("default");
   const [contactContents, setContactContents] = useState<string>("");
 
   const navigate = useNavigate();
 
-  // const handleInquiryTypeChange = (event: any) => {
-  //   setInquiryType(event.target.value as string);
-  // };
-
-  const onClickSubmit = () => {
-    navigate("/home");
+  const onClickSubmit = async () => {
+    console.log("start");
+    await fetch("http://localhost:8880/contact", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        category: contactCategory,
+        content: contactContents,
+      }),
+    }).then(() => {
+      navigate("/home");
+    });
   };
 
   return (
@@ -50,53 +43,15 @@ const Contact: FC<Props> = memo((props) => {
         >
           お問い合わせ
         </Typography>
-        <Box sx={{ mb: "30px" }}>
-          <FormControl variant="standard" error={contactCategory === 0}>
-            <InputLabel id="itemCategoryField" required>
-              お問い合わせ種別
-            </InputLabel>
-            <Select
-              labelId="contactCategoryField"
-              id="contactCategoryField"
-              value={contactCategory}
-              label="お問い合わせ種別"
-              onChange={(e) => {
-                setContactCategory(Number(e.target.value));
-              }}
-            >
-              <MenuItem value={0}>お問い合わせ種別を選択</MenuItem>
-              <MenuItem value={1}>システムについてのお問い合わせ</MenuItem>
-              <MenuItem value={2}>サービスについてのお問い合わせ</MenuItem>
-              <MenuItem value={3}>
-                コーヒーサーバについてのお問い合わせ
-              </MenuItem>
-              <MenuItem value={4}>その他</MenuItem>
-            </Select>
-            {contactCategory === 0 && (
-              <FormHelperText>お問い合わせ種別を選択して下さい</FormHelperText>
-            )}
-          </FormControl>
-        </Box>
-        <PrimaryInput
-          multiline
-          aria-label="contactContents"
-          label="お問い合わせ内容"
-          sx={{ width: "100%", mb: "60px" }}
-          value={contactContents}
-          required
-          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-            setContactContents(e.target.value)
-          }
-          rows={5}
-          helperText={
-            contentErr && contactContents === "" && "お問い合わせ内容を入力して下さい"
-          }
-          error={contentErr && contactContents === ""}
-          onBlur={() => setContentErr(true)}
+        <ContactForm
+          setContactCategory={setContactCategory}
+          contactCategory={contactCategory}
+          setContactContents={setContactContents}
+          contactContents={contactContents}
         />
 
         <Box sx={{ display: "flex", justifyContent: "center", mb: "60px" }}>
-          {contactCategory === 0 ? (
+          {contactCategory === "default" || contactContents === "" ? (
             <InactiveButton
               sxStyle={{
                 px: 7,
