ecc34b5b498d8b96386cc82b423139d78d4beffe
Kazuaki Kuwahara <kazuaki.kuwahara@rakus-partners.co.jp>
2023-07-05 09:54:13 +0900

    merge main

---
+++ src/__test__/Login.test.tsx
@@ -0,0 +1,165 @@
+import { render, screen, fireEvent, act, waitFor } from '@testing-library/react';
+import LoginForm from '../components/organisms/LoginForm';
+import { BrowserRouter, MemoryRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import { MainRouter } from '../router/MainRouter';
+import Top from '../components/pages/Top';
+import EmailInput from '../components/atoms/login/EmailInput';
+import { FocusEvent } from 'react';
+import userEvent from '@testing-library/user-event';
+
+// yarn test src/components/LoginForm.test.js
+
+describe('LoginForm', () => {
+  test('title表示', () => {
+    const loginTitle = 'Login';
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <LoginForm loginTitle={loginTitle} />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const titleElement = screen.getByText(loginTitle);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test('ボタン表示', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <LoginForm loginTitle="Login" />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const buttons = await screen.findAllByRole("button");
+    expect(buttons).toHaveLength(2);
+  });
+
+  test('存在しないユーザのバリデーション表示', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <LoginForm loginTitle='Login' />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const emailInput = screen.getByPlaceholderText('example@rakus-partners.co.jp');
+    const passwordInput = screen.getByPlaceholderText('パスワード');
+    const submitButton = screen.getByRole('button', { name: 'ログイン' });
+    fireEvent.change(emailInput, { target: { value: 'test@example.com' } });
+    fireEvent.change(passwordInput, { target: { value: 'Password123' } });
+    fireEvent.click(submitButton);
+    const errorMessage = await screen.findByText('ユーザーが存在しません');
+    expect(errorMessage).toBeInTheDocument();
+  });
+
+  test('emailinputが空の場合のエラー', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <LoginForm loginTitle='Login' />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const emailInput = screen.getByPlaceholderText('example@rakus-partners.co.jp');
+    fireEvent.blur(emailInput);
+    const errorUserMessage = screen.getByText('メールアドレスを入力してください');
+    expect(errorUserMessage).toBeVisible();
+  });
+
+
+  test('emailinputに@が入っていない場合のエラー', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+           <LoginForm loginTitle='Login' />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const emailInput = screen.getByPlaceholderText('example@rakus-partners.co.jp') as HTMLInputElement;
+    fireEvent.change(emailInput, { target: { value: 'email' } });
+    fireEvent.blur(emailInput); 
+    expect(emailInput.value).toBe('email');
+    const errorText = screen.queryByText('＠を含んだ40文字以内で入力してください');
+    expect(errorText).toBeInTheDocument();
+  });
+
+  test('passwordinputが空の場合のエラー', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <LoginForm loginTitle='Login' />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const passwordInput = screen.getByPlaceholderText('パスワード');
+    fireEvent.blur(passwordInput);
+    const errorUserMessage = screen.getByText('パスワードを入力してください');
+    expect(errorUserMessage).toBeVisible();//表示されているか
+  });
+
+  test('typeがpasswordになっているか', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <LoginForm loginTitle='Login' />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const passwordInput = screen.getByPlaceholderText('パスワード');
+    expect(passwordInput).toHaveAttribute("type","password");
+  });
+
+  test('passwordinputのその他エラーメッセージ表示', () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <LoginForm loginTitle='Login' />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    // パスワード入力要素を取得
+    const passwordInput = screen.getByPlaceholderText('パスワード')as HTMLInputElement;
+    // パスワードを入力
+    fireEvent.change(passwordInput, { target: { value: 'pass123' } });
+    fireEvent.blur(passwordInput); 
+    expect(passwordInput.value).toBe('pass123');
+    const errorMessage = screen.getByText(/半角英字大文字、小文字、数字の3種類を1つ必ず使用/);
+    expect(errorMessage).toBeInTheDocument();
+  
+    fireEvent.blur(passwordInput);
+  
+    fireEvent.focus(passwordInput);
+  
+    const elements = screen.getAllByText(/8文字以上16文字以内/);
+    const errorTextElement = elements[0];
+    expect(errorTextElement).toBeInTheDocument();
+    const elements2 = screen.getAllByText(
+      /半角英字大文字、小文字、数字の3種類を1つ必ず使用/
+    );
+    const errorTextElement2=elements2[0];
+    expect(errorTextElement2).toBeInTheDocument();
+  });
+
+  test('正常なログイン処理', async () => {
+    render(
+      <MemoryRouter initialEntries={["/login"]}>
+        <RecoilRoot>
+        <LoginForm loginTitle='Login' />
+        <Top/>
+        </RecoilRoot>
+      </MemoryRouter>
+    );
+    const submitButton=screen.getByTestId("submit");
+    const emailInput = screen.getByPlaceholderText('example@rakus-partners.co.jp')as HTMLInputElement;
+    const passwordInput = screen.getByPlaceholderText('パスワード')as HTMLInputElement;
+    userEvent.type(emailInput,"example2@rakus-partners.co.jp")
+    userEvent.type(passwordInput,"Example2")
+    userEvent.click(submitButton);
+
+    await waitFor(() => {
+      expect(screen.getByRole('button', { name: 'ログアウト' })).toBeInTheDocument();
+    });
+  });
+})
--- src/__test__/poll/AddPoll.test.tsx
+++ src/__test__/poll/AddPoll.test.tsx
@@ -1,1 +1,177 @@
+import React from 'react';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+import AddPoll from '../../components/pages/AddPoll';
+import { BrowserRouter } from 'react-router-dom';
+import { RecoilRoot } from 'recoil';
+import AdminHome from '../../components/pages/AdminHome';
+import AddPollCard from '../../components/organisms/card/AddPollCard';
 
+describe('AddPoll', () => {
+  const testItemData = [
+    {
+      "itemName": "ポテトチップスのりしお",
+      "description": "ふわっと香る「青のり」が、じゃがいものおいしさをいっそう引き立てます。",
+      "image": [
+        "https://firebasestorage.googleapis.com/v0/b/drink-manager-app-4df85.appspot.com/o/8b4f2c9d-a9d6-45f0-bf8f-b5bd96ec4461?alt=media&token=96f2cbda-a7ba-45e5-aba7-70cfe8fbcd5f"
+      ],
+      "itemCategory": 7,
+      "createdAt": new Date("2023-05-12T03:01:31.537Z"),
+      "intheOffice": false,
+      "author": null,
+      "otherItem": true,
+      "id": 25
+    },
+    {
+      "itemName": "ビスコ",
+      "description": "ふわっと香る「青のり」が、じゃがいものおいしさをいっそう引き立てます。",
+      "image": [
+        "https://firebasestorage.googleapis.com/v0/b/drink-manager-app-4df85.appspot.com/o/8b4f2c9d-a9d6-45f0-bf8f-b5bd96ec4461?alt=media&token=96f2cbda-a7ba-45e5-aba7-70cfe8fbcd5f"
+      ],
+      "itemCategory": 7,
+      "createdAt": new Date("2023-05-12T03:01:31.537Z"),
+      "intheOffice": false,
+      "author": null,
+      "otherItem": true,
+      "id": 26
+    }
+  ];
+  test('pollNameInput空文字エラー', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <AddPoll />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const pollNameInput = screen.getByPlaceholderText('投票タイトル') as HTMLInputElement;
+
+    const submitButton = screen.getByRole('button', { name: '投票登録' }) as HTMLButtonElement;
+    fireEvent.click(submitButton);
+    expect(pollNameInput.value).toBe('');
+    const errorUserMessage = screen.queryAllByText('投票名を入力してください');
+    errorUserMessage.forEach(element => {
+      expect(element).toBeVisible();
+    });
+  });
+  test('pollDescriptionInput空文字エラー', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <AddPoll />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const pollDetailInput = screen.getByPlaceholderText('投票詳細') as HTMLInputElement;
+
+    const submitButton = screen.getByRole('button', { name: '投票登録' }) as HTMLButtonElement;
+    fireEvent.click(submitButton);
+    expect(pollDetailInput.value).toBe('');
+    const errorUserMessage = screen.queryAllByText('投票詳細を入力してください');
+    errorUserMessage.forEach(element => {
+      expect(element).toBeVisible();
+    });
+  });
+  test('pollDateInput空文字エラー', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <AddPoll />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const pollDateInput = screen.getByPlaceholderText('日付') as HTMLInputElement;
+    const pollDateInput2 = screen.getByPlaceholderText('日付2') as HTMLInputElement;
+
+    const submitButton = screen.getByRole('button', { name: '投票登録' }) as HTMLButtonElement;
+    fireEvent.click(submitButton);
+    expect(pollDateInput.value).toBe('');
+    expect(pollDateInput2.value).toBe('');
+    const errorUserMessage = screen.queryAllByText('投票期間を入れてください');
+    errorUserMessage.forEach(element => {
+      expect(element).toBeVisible();
+    });
+  });
+  test('pollcategory未選択エラー', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <AddPoll />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const submitButton = screen.getByRole('button', { name: '投票登録' }) as HTMLButtonElement;
+    fireEvent.click(submitButton);
+    const errorUserMessage = screen.queryAllByText('投票種別を選択してください');
+    errorUserMessage.forEach(element => {
+      expect(element).toBeVisible();
+    });
+  });
+  test('pollCard未選択エラー', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <AddPoll />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const submitButton = screen.getByRole('button', { name: '投票登録' }) as HTMLButtonElement;
+    fireEvent.click(submitButton);
+    const errorUserMessage = screen.queryAllByText('投票に追加する商品を選択してください');
+    errorUserMessage.forEach(element => {
+      expect(element).toBeVisible();
+    });
+  });
+
+  test('投票登録ボタンがクリックされたとき入力が正常であれば投票情報が登録される', async () => {
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <AddPoll />
+          <AdminHome />
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+
+    // 必要な要素を取得
+    const pollNameInput = screen.getByPlaceholderText('投票タイトル') as HTMLInputElement;
+    const pollDescriptionInput = screen.getByPlaceholderText('投票詳細') as HTMLInputElement;
+    // const pollCategorySelect = screen.getByRole('combobox', { name: '投票種別を選択してください' });
+    const startDateInput = screen.getByPlaceholderText('日付') as HTMLInputElement;
+    const endDateInput = screen.getByPlaceholderText('日付2') as HTMLInputElement;
+    const submitButton = screen.getByRole('button', { name: '投票登録' });
+
+    fireEvent.change(pollNameInput, { target: { value: 'テスト投票' } });
+    expect(pollNameInput.value).toBe('テスト投票');
+    fireEvent.change(pollDescriptionInput, { target: { value: 'テスト投票の詳細' } });
+    // fireEvent.change(pollCategorySelect, { target: { value: '1' } });
+    fireEvent.change(startDateInput, { target: { value: '2023-06-1' } });
+    fireEvent.change(endDateInput, { target: { value: '2023-06-30' } });
+
+    fireEvent.click(submitButton);
+
+    await waitFor(() => {
+      const titleElement = screen.getByText("- 管理者MENU -");
+      expect(titleElement).toBeInTheDocument();
+    });
+  });
+
+  test("テストデータのカードが表示できるか", () => {
+
+    render(
+      <RecoilRoot>
+        <BrowserRouter>
+          <AddPollCard data={testItemData} selectedItems={[]} setSelectedItems={[26]}/>
+        </BrowserRouter>
+      </RecoilRoot>
+    );
+    const item1 = screen.getByText((content, element) => {
+      return content.startsWith("ポテトチップスのりしお");
+    });
+    expect(item1).toBeInTheDocument();
+
+    const item2 = screen.getByText((content, element) => {
+      return content.startsWith("ビスコ");
+    });
+    expect(item2).toBeInTheDocument();
+  });
+});
